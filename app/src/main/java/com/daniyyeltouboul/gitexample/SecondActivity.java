1ce355db95e1b621d68ca5f6a22b017e73acc51e
Daniyyel Touboul <danieltubul@gmail.com>
2017-07-30 13:48:30 +0300

    Initial commit

---
+++ app/src/main/java/com/daniyyeltouboul/gitexample/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.daniyyeltouboul.gitexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Daniyyel on 7/30/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+
+
+    }
+}
